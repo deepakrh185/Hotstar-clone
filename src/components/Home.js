7c4b1df4de2929a3508e72f0c46e29fea8a8d854
Deepak <deepsrh1801@gmail.com>
2021-05-20 21:05:43 +0530

    added slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Container>
+      <ImgSlider />
+    </Container>
+  );
 }
 
 export default Home;
 
-
-
-
-
-
-
+const Container = styled.main`
+  height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+`;
